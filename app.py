3e4d9b270cf2fa16d6927ea40f5fbe468f5f8340
kartik poojari <kartikpoojari29@gmail.com>
2024-03-20 20:39:40 +0530

    changes

--- app.py
+++ app.py
@@ -1,84 +1,97 @@
-from flask import Flask, render_template, redirect, url_for, session, flash
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-from flask_mysqldb import MySQL
-import yaml
+from flask import Flask, request, render_template, redirect, session, send_file
+from flask_sqlalchemy import SQLAlchemy
+import bcrypt
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databse.db'
+db = SQLAlchemy(app)
+app.secret_key = 'secrete_key'
 
-db = yaml.safe_load(open('db.yaml'))
-app.config['MYSQL_HOST'] = db['host']
-app.config['MYSQL_USER'] = db['user']
-app.config['MYSQL_PASSWORD'] = db['password']
-app.config['MYSQL_DB'] = db['db']
-app.secret_key = 'mysecretkey'
-mysql = MySQL(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
 
-class LoginForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Login")
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-@app.route('/', methods = ['GET', 'POST'])
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    restaurantName = db.Column(db.String(100), nullable=False)
+    productName = db.Column(db.String(100), nullable=False)
+    productImage = db.Column(db.LargeBinary)
+
+    def __init__(self, restaurantName, productName, productImage):
+        self.restaurantName = restaurantName
+        self.productName = productName
+        self.productImage = productImage
+
+
+with app.app_context():
+    db.create_all()
+
+admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Product, db.session))
+
+@app.route('/')
+def index():
+    return render_template('register.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        new_user = User(name=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect('/login')
+    else:
+        return render_template('register.html', error='Email Already Registered')
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
 
-        #hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        user = User.query.filter_by(email=email).first()
 
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM users where username=%s", (username,))
-        user = cursor.fetchone()       
-        mysql.connection.commit()
-        cursor.close()
-
-        if user and password == user[2]:
-            if user[3] == 'admin':
-                session['id'] = user[0]
-                return redirect(url_for('admindashboard'))
-            else:
-                session['id'] = user[0]
-                return redirect(url_for('dashboard'))
+        if user and user.check_password(password):
+            session['email'] = user.email
+            return redirect('/dashboard')
         else:
-            flash("Login failed. Invalid credentials.")
-            return redirect(url_for('login'))
+            return render_template('login.html', error = 'Invalid User')
 
-    return render_template('login.html', form=form)
+    return render_template('login.html')
 
-@app.route('/dashboard', methods = ['GET', 'POST'])
+@app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
-    if 'id' in session:
-        id = session['id']
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM users where id=%s", (id,))
-        user = cursor.fetchone()
-        cursor.close()
+    if session['email']:
+        user = User.query.filter_by(email=session['email']).first()
+        return render_template('a.html', user=user)
+    return redirect('/login')
 
-        if user:
-            return render_template('dashboard.html', user = user)
-    return redirect(url_for('login'))
 
-@app.route('/admindashboard', methods = ['GET', 'POST'])
-def admindashboard():
-    if 'id' in session:
-        id = session['id']
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM users where id=%s", (id,))
-        user = cursor.fetchone()
-        cursor.close()
-
-        if user:
-            return render_template('admindashboard.html', user = user)
-    return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
-    session.pop('id', None)
-    flash("Logged out successfully!")
-    return redirect(url_for('login'))
+    session.pop('email', None)
+    return redirect('/login')
 
-if __name__=="__main__":
+if __name__ == '__main__':
     app.run(debug=True)
