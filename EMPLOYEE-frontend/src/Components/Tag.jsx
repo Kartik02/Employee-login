e967626812cb1aaba538a1e17e296409b8c77fbf
kartik poojari <kartikpoojari29@gmail.com>
2024-05-03 16:07:05 +0530

    time tracker update

--- EMPLOYEE-frontend/src/Components/Tag.jsx
+++ EMPLOYEE-frontend/src/Components/Tag.jsx
@@ -1,89 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+
 
 function Tag() {
+  // State to hold the input value and submitted values
   const [inputValue, setInputValue] = useState('');
-  const [tags, setTags] = useState([]);
-  const [error, setError] = useState(null);
-  const [successMessage, setSuccessMessage] = useState('');
+  const [submittedValues, setSubmittedValues] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(() => {
-    // Fetch tags from Flask backend when component mounts
-    axios.get('http://localhost:5000/api/tag_list')
-      .then(response => {
-        setTags(response.data.tags);
-      })
-      .catch(error => {
-        setError(error.message);
-      });
-  }, []);
-
+  // Function to handle input change
   const handleChange = (e) => {
     setInputValue(e.target.value);
   };
 
+  // Function to handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
     if (inputValue.trim() !== '') {
-      // Send the new tag to Flask backend
-      axios.post('http://localhost:5000/api/add_tag', { tag: inputValue })
-        .then(response => {
-          if (response.data.message) {
-            // Add the new tag to the local state
-            setTags([...tags, { id: tags.length + 1, tag: inputValue }]);
-            setInputValue('');
-            setError(null);
-            setSuccessMessage('Tag added successfully!');
-          } else {
-            setError(response.data.error);
-          }
-        })
-        .catch(error => {
-          setError(error.message);
-        });
+      setSubmittedValues([...submittedValues, inputValue]);
+      setInputValue('');
     }
   };
 
+  // Function to handle search input change
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filter submitted values based on search term
+  const filteredValues = submittedValues.filter(value =>
+    value.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="container">
-      <p style={{ fontSize: '40px' }}>Tags</p>
-      {error && <p>Error: {error}</p>}
-      {successMessage && <p className="text-success">{successMessage}</p>}
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-auto">
-            <label htmlFor="inputTag" className="visually-hidden">Tag</label>
-            <input
-              type="text"
-              className="form-control mb-3"
-              id="inputTag"
-              placeholder="Add new tag"
-              value={inputValue}
-              onChange={handleChange}
-            />
+    
+      <p style={{fontSize:'40px'}}>Tags</p>
+      <div className='row main-box' style={{ display: 'block', marginTop: '20px' }}>
+        <form onSubmit={handleSubmit}>
+          <div className="row " style={{ float: 'right' }}>
+            <div className="col-auto">
+              <label htmlFor="inputTag" className="visually-hidden">Tag</label>
+              <input
+                type="text"
+                className="form-control mb-3"
+                id="inputTag"
+                placeholder="Add new tag"
+                value={inputValue}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary mb-3">Add Tag</button>
+            </div>
           </div>
-          <div className="col-auto">
-            <button type="submit" className="btn btn-primary mb-3">Add Tag</button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
+      <div className='Searchbar col-md-3' >
+        <input type="text" className='form-control mb-3' placeholder="Search By Name" id='Search' value={searchTerm} onChange={handleSearchChange} />
+      </div>
       <table className="table mt-3 table">
         <thead>
           <tr>
             <th>#</th>
-            <th>Tag</th>
           </tr>
         </thead>
         <tbody>
-          {tags.map((tag, index) => (
+          {filteredValues.map((value, index) => (
             <tr key={index}>
-              <td>{tag.id}</td>
-              <td>{tag.tag}</td>
+              <td>{value} <span className='edit' style={{ float: 'right' }}>
+              <i class="bi bi-pencil-fill" style={{ marginRight: '15px' }}> </i> 
+              <i class="bi bi-three-dots-vertical"></i>
+              </span>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
-    </div>
+      </div>
+    
   );
 }
 
--- EMPLOYEE-frontend/src/Components/TimeTracker.jsx
+++ EMPLOYEE-frontend/src/Components/TimeTracker.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
-import axios from 'axios';
+import React, { useState, useRef, useEffect } from "react";
+import Reports from "./Reports";
 
 const ProjectDropdown = ({ projectName, setProjectName, projects }) => {
   const handleProjectSelect = (selectedProject) => {
@@ -7,16 +7,17 @@ const ProjectDropdown = ({ projectName, setProjectName, projects }) => {
   };
 
   return (
-    <div className="relative flex-1">
+    <div className="tw-relative tw-flex-1">
+      {/* Project dropdown */}
       <select
         value={projectName}
         onChange={(e) => setProjectName(e.target.value)}
-        className="border border-gray-500 px-2 py-1 mr-2 flex-1"
+        className="tw-border tw-border-gray-500 tw-px-2 tw-py-1 tw-mr-2 tw-flex-1"
       >
         <option value="">Select Project</option>
         {projects.map((project, index) => (
-          <option key={index} value={project}>
-            {project}
+          <option key={index} value={project.projectName}>
+            {project.projectName}
           </option>
         ))}
       </select>
@@ -24,54 +25,16 @@ const ProjectDropdown = ({ projectName, setProjectName, projects }) => {
   );
 };
 
-const TagListDropdown = ({ tags, selectedTags, handleTagSelect }) => {
-  const [showDropdown, setShowDropdown] = useState(false);
-
-  const handleTagClick = () => {
-    setShowDropdown(!showDropdown);
-  };
-
-  const handleCheckboxChange = (tag) => {
-    handleTagSelect(tag);
-  };
-
-  return (
-    <div className="relative inline-block">
-      <button
-        className="border border-gray-500 px-2 py-1 cursor-pointer"
-        onClick={handleTagClick}
-        style={{ borderBottomLeftRadius: 0, borderTopLeftRadius: 0 }} // Adjust border radius to match the Project dropdown button
-      >
-        TagList
-      </button>
-      {showDropdown && (
-        <div className="absolute mt-1 bg-white shadow-md rounded-md border border-gray-500" style={{ width: '150px' }}>
-          {tags.map((tag, index) => (
-            <label key={index} className="block px-3 py-2 cursor-pointer hover:bg-gray-200">
-              <input
-                type="checkbox"
-                checked={selectedTags.includes(tag)}
-                onChange={() => handleCheckboxChange(tag)}
-                className="mr-2 cursor-pointer"
-              />
-              {tag.tag}
-            </label>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
-
 const Stopwatch = () => {
-  const [task, setTask] = useState("");
+  const [task, setTask] = useState(""); // State for the task description
   const [projectName, setProjectName] = useState("");
-  const [selectedTags, setSelectedTags] = useState([]);
+  const [tags, setTags] = useState([]); // State for storing selected tags
   const [timeElapsed, setTimeElapsed] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const [pausedTime, setPausedTime] = useState(0);
   const [projects, setProjects] = useState([]);
-  const [tags, setTags] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false); // State to control the visibility of the dropdown
+
   const intervalRef = useRef();
 
   useEffect(() => {
@@ -81,30 +44,8 @@ const Stopwatch = () => {
     return () => {
       clearInterval(intervalRef.current);
     };
+    // eslint-disable-next-line
   }, [isRunning]);
-
-  useEffect(() => {
-    fetchProjects();
-    fetchTags(); // Fetch tags when component mounts
-  }, []);
-
-  const fetchProjects = async () => {
-    try {
-      const response = await axios.get('http://localhost:5000/api/project_list');
-      setProjects(response.data);
-    } catch (error) {
-      console.error('Error fetching projects:', error);
-    }
-  };
-
-  const fetchTags = async () => {
-    try {
-      const response = await axios.get('http://localhost:5000/api/tag_list');
-      setTags(response.data.tags); // Assuming the response contains an array of tag objects with a 'tag' property
-    } catch (error) {
-      console.error('Error fetching tags:', error);
-    }
-  };
 
   const startTimer = () => {
     const startTime = Date.now() - pausedTime;
@@ -116,6 +57,12 @@ const Stopwatch = () => {
   const stopTimer = () => {
     clearInterval(intervalRef.current);
     setPausedTime(timeElapsed);
+  };
+
+  const resetTimer = () => {
+    clearInterval(intervalRef.current);
+    setTimeElapsed(0);
+    setPausedTime(0);
   };
 
   const handleStart = () => {
@@ -140,18 +87,72 @@ const Stopwatch = () => {
   };
 
   const handleReset = () => {
-    clearInterval(intervalRef.current);
-    setTimeElapsed(0);
-    setPausedTime(0);
+    resetTimer();
+    setTask("");
+    setProjectName("");
+    setTags([]);
+    setIsRunning(false);
+  };
+
+  const handleSubmit = () => {
+    if (!task.trim()) {
+      alert("Task description is required!");
+      return;
+    }
+    if (!projectName.trim()) {
+      alert("Project name is required!");
+      return;
+    }
+    const newProject = {
+      task,
+      projectName,
+      tags, // Include selected tags in the project object
+      timeElapsed: Math.floor(timeElapsed / 1000), // Convert milliseconds to seconds
+    };
+
+    fetch('https://empbackend.vercel.app/api/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newProject)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Failed to add project');
+        }
+      })
+      .then(data => {
+        console.log(data.message);
+        setProjects([...projects, newProject]);
+        handleReset();
+        // Navigate to ProjectDetailsPage with projectName and timeElapsed
+        // Assuming you have some kind of routing mechanism like react-router
+        // Replace the navigation logic according to your routing library
+        navigateToProjectDetailsPage(projectName, Math.floor(timeElapsed / 1000));
+      })
+      .catch(error => {
+        console.error('Error adding project:', error);
+        alert('Failed to add project');
+      });
+
+  };
+
+  const handleTagClick = () => {
+    setShowDropdown(!showDropdown); // Toggle the visibility of the dropdown
   };
 
   const handleTagSelect = (selectedTag) => {
-    if (selectedTags.includes(selectedTag)) {
-      setSelectedTags(selectedTags.filter(tag => tag !== selectedTag));
+    // Toggle the tag in the selected tags array
+    if (tags.includes(selectedTag)) {
+      setTags(tags.filter(tag => tag !== selectedTag));
     } else {
-      setSelectedTags([...selectedTags, selectedTag]);
+      setTags([...tags, selectedTag]);
     }
   };
+
 
   const formatTime = (milliseconds) => {
     const hours = Math.floor(milliseconds / (1000 * 60 * 60));
@@ -162,20 +163,126 @@ const Stopwatch = () => {
   };
 
   return (
-    <div className="tw-flex tw-flex-col tw-items-center tw-p-4">
-      <div className="tw-mb-4 tw-border-2 tw-border-black tw-rounded tw-p-2 tw-flex tw-w-full">
-        <input
-          type="text"
-          placeholder="What are you working on?"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-          className="tw-border tw-border-gray-500 tw-px-2 tw-py-1 tw-mr-2 tw-flex-1"
-          style={{ borderBottomRightRadius: 0, borderTopRightRadius: 0 }} // Adjust border radius for left input
-        />
-        <ProjectDropdown projectName={projectName} setProjectName={setProjectName} projects={projects} />
-        <TagListDropdown tags={tags} selectedTags={selectedTags} handleTagSelect={handleTagSelect} />
+    <>
+      <div className="tw-flex tw-flex-col tw-items-center tw-p-4">
+        <div className="tw-mb-4 tw-border-2 tw-border-black tw-rounded tw-p-2 tw-flex tw-w-full">
+          <input
+            type="text"
+            placeholder="What are you working on?"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            className="tw-border tw-border-gray-500 tw-px-2 tw-py-1 tw-mr-2 tw-flex-1"
+            style={{ borderBottomRightRadius: 0, borderTopRightRadius: 0 }} // Adjust border radius for left input
+          />
+          <ProjectDropdown projectName={projectName} setProjectName={setProjectName} projects={projects} />
+          <div className="tw-absolute tw-right-80 ">
+            {/* Tag symbol */}
+            <div
+              className="tw-border tw-border-gray-500 tw-px-2 tw-py-1 tw-cursor-pointer"
+              onClick={handleTagClick} // Handle click to show dropdown
+              style={{ marginRight: '8px' }} // Reduce space between tag and project
+            >
+              {tags.length > 0 ? tags.join(", ") : <i class="bi bi-tag"></i>}
+            </div>
+            {/* Dropdown */}
+            {showDropdown && (
+              <div className="tw-absolute tw-mt-1 tw-bg-white tw-shadow-md tw-rounded-md">
+                <ul>
+                  <li className="tw-cursor-pointer tw-px-3 tw-py-2 tw-hover:bg-gray-200">
+                    <input
+                      type="checkbox"
+                      value="Frontend"
+                      checked={tags.includes("Frontend")}
+                      onChange={() => handleTagSelect("Frontend")}
+                    />
+                    Frontend
+                  </li>
+                  <li className="tw-cursor-pointer tw-px-3 tw-py-2 tw-hover:bg-gray-200">
+                    <input
+                      type="checkbox"
+                      value="Backend"
+                      checked={tags.includes("Backend")}
+                      onChange={() => handleTagSelect("Backend")}
+                    />
+                    Backend
+                  </li>
+                  <li className="tw-cursor-pointer tw-px-3 tw-py-2 tw-hover:bg-gray-200">
+                    <input
+                      type="checkbox"
+                      value="Database"
+                      checked={tags.includes("Database")}
+                      onChange={() => handleTagSelect("Database")}
+                    />
+                    Database
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+
+          {!isRunning ? (
+            <button
+              onClick={handleStart}
+              disabled={!task.trim() || !projectName.trim()} // Disable button if task or projectName is empty or contains only whitespace
+              className={`tw-bg-blue-500 tw-hover:bg-blue-700 tw-text-white tw-font-bold tw-py-1 tw-px-4 tw-rounded ${(!task.trim() || !projectName.trim()) && "tw-opacity-50 tw-cursor-not-allowed"}`}
+              style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }} // Adjust border radius for start button
+            >
+              Start
+            </button>
+          ) : (
+            <>
+              <button
+                onClick={handlePause}
+                className="tw-bg-red-500 tw-hover:bg-red-700 tw-text-white tw-font-bold tw-py-1 tw-px-4 tw-rounded tw-mr-2"
+              >
+                Pause
+              </button>
+              <button
+                onClick={handleReset}
+                className="tw-bg-gray-500 tw-hover:bg-gray-700 tw-text-white tw-font-bold tw-py-1 tw-px-4 tw-rounded"
+              >
+                Reset
+              </button>
+            </>
+          )}
+        </div>
+        <div>
+          <h2 className="tw-font-bold">Time Taken: {formatTime(timeElapsed)}</h2>
+        </div>
+        {projects.length > 0 && (
+          <div className="tw-mb-4">
+            <h2 className="tw-font-bold">Project History:</h2>
+            <ul>
+              {projects.map((project, index) => (
+                <li key={index}>
+                  Project Name: {project.projectName}, Tags: {project.tags.join(", ")}, Time
+                  Elapsed: {project.timeElapsed} seconds
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        <div>
+          {isRunning && (
+            <button
+              onClick={handleSubmit}
+              className="tw-bg-green-500 tw-hover:bg-green-700 tw-text-white tw-font-bold tw-py-1 tw-px-4 tw-rounded"
+            >
+              Submit
+            </button>
+          )}
+          {!isRunning && pausedTime > 0 && (
+            <button
+              onClick={handleResume}
+              className="tw-bg-blue-500 tw-hover:bg-blue-700 tw-text-white tw-font-bold tw-py-1 tw-px-4 tw-rounded"
+            >
+              Resume
+            </button>
+          )}
+        </div>
+
       </div>
-    </div>
+    </>
   );
 };
 
