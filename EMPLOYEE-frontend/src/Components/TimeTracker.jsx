f285c31953bcdc23b3b05d18f13e22576245051d
salaudeen22 <salaudeensalu@gmail.com>
2024-06-22 09:31:56 +0530

    minor changes due to conflicts

--- EMPLOYEE-frontend/src/Components/TimeTracker.jsx
+++ EMPLOYEE-frontend/src/Components/TimeTracker.jsx
@@ -1,116 +1,87 @@
 import React, { useState, useEffect, useRef } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
 const Stopwatch = () => {
   const [task, setTask] = useState("");
   const [projectName, setProjectName] = useState("");
   const [tags, setTags] = useState([]);
   const [timeElapsed, setTimeElapsed] = useState(0);
+  const [date, setDate] = useState("");
   const [isRunning, setIsRunning] = useState(false);
   const [pausedTime, setPausedTime] = useState(0);
   const [projects, setProjects] = useState([]);
   const [showDropdown, setShowDropdown] = useState(false);
   const [submittedDetails, setSubmittedDetails] = useState([]);
   const [editIndex, setEditIndex] = useState(null);
-  const [hoveredRowIndex, setHoveredRowIndex] = useState(null);
-  const [hoveredColumnIndex, setHoveredColumnIndex] = useState(null);
-  const [filterDate, setFilterDate] = useState(null);
   const intervalRef = useRef();
 
-  // // Dummy data
-  // const dummyProjects = ["Project A", "Project B", "Project C"];
-  // const dummyTags = [
+  const [expandedRows, setExpandedRows] = useState({});
 
-  //   { name: "aaa", checked: false },
-  //   { name: "bbb", checked: false },
-  //   { name: "ccc", checked: false },
-  //   { name: "ddd", checked: false },
-  //   { name: "eee", checked: false },
-  // ];
-  // const dummySubmittedDetails = [
-  //   {
-  //     projectName: "Project A",
-  //     task: "Initial Setup",
-  //     tags: ["aaa"],
-  //     timeTaken: "01:00:00",
-  //     projectid: 1,
-  //     createdDate: new Date("2024-06-11"),
-  //   },
-  // ];
+  const toggleRowExpansion = (index) => {
+    if (expandedRows[index]) {
+      clearTimeout(expandedRows[index].timer);
+      setExpandedRows((prev) => {
+        const newExpandedRows = { ...prev };
+        delete newExpandedRows[index];
+        return newExpandedRows;
+      });
+    } else {
+      const timer = setTimeout(() => {
+        setExpandedRows((prev) => {
+          const newExpandedRows = { ...prev };
+          delete newExpandedRows[index];
+          return newExpandedRows;
+        });
+      }, 3000);
+
+      setExpandedRows((prev) => ({
+        ...prev,
+        [index]: { timer },
+      }));
+    }
+  };
 
   useEffect(() => {
     fetchProjects();
     fetchTags();
-    fetchEmployeeProjects();
+    fetchEmployeeProjects(); // Fetch employee projects when component mounts
   }, []);
-
-  // const fetchProjects = () => {
-  //   setTimeout(() => {
-  //     setProjects(dummyProjects);
-  //   }, 500); // Simulate API delay
-  // };
-
-  // const fetchTags = () => {
-  //   setTimeout(() => {
-  //     setTags(dummyTags);
-  //   }, 500); // Simulate API delay
-  // };
-
-  // const fetchEmployeeProjects = () => {
-  //   setTimeout(() => {
-  //     setSubmittedDetails(dummySubmittedDetails);
-  //   }, 500); // Simulate API delay
-  // };
 
   const fetchProjects = async () => {
     try {
-<<<<<<< HEAD
-      const response = await axios.get('https://rmbackend.vercel.app/auth/project_list');
-=======
       const response = await axios.get(
-        "https://rmbackend.vercel.app/auth/project_list"
+        "https://ten-tuuo.onrender.com/auth/project_list"
       );
->>>>>>> 1cf743038ac42a86793584312849e84d38676e2a
       setProjects(response.data);
     } catch (error) {
-      console.error('Error fetching projects:', error);
+      console.error("Error fetching projects:", error);
     }
   };
 
   const fetchTags = async () => {
     try {
-<<<<<<< HEAD
-      const response = await axios.get('https://rmbackend.vercel.app/auth/tag_list');
-      setTags(response.data.tags.map(tag => ({ name: tag.tag, checked: false })));
-=======
       const response = await axios.get(
-        "https://rmbackend.vercel.app/auth/tag_list"
+        "https://ten-tuuo.onrender.com/auth/tag_list"
       );
       setTags(
         response.data.tags.map((tag) => ({ name: tag.tag, checked: false }))
       );
->>>>>>> 1cf743038ac42a86793584312849e84d38676e2a
     } catch (error) {
-      console.error('Error fetching tags:', error);
+      console.error("Error fetching tags:", error);
     }
   };
 
   const fetchEmployeeProjects = async () => {
     try {
-<<<<<<< HEAD
-      const response = await axios.get('https://rmbackend.vercel.app/auth/get_employee_projects', { withCredentials: true });
-=======
       const response = await axios.get(
-        "https://rmbackend.vercel.app/auth/get_employee_projects",
+        "https://ten-tuuo.onrender.com/auth/get_employee_projects",
         { withCredentials: true }
       );
->>>>>>> 1cf743038ac42a86793584312849e84d38676e2a
       const employeeProjects = response.data.projects;
       // Assuming the employeeProjects data format is similar to the submittedDetails state
       setSubmittedDetails(employeeProjects);
     } catch (error) {
-      console.error('Error fetching employee projects:', error);
+      console.error("Error fetching employee projects:", error);
     }
   };
 
@@ -148,13 +119,14 @@ const Stopwatch = () => {
     setIsRunning(false);
     setTask("");
     setProjectName("");
-    setTags(tags.map(tag => ({ ...tag, checked: false })));
+    setTags(tags.map((tag) => ({ ...tag, checked: false })));
   };
 
   const handleRun = (index) => {
     if (isRunning) return; // Prevent running if already running
     setEditIndex(index);
-    const startTime = Date.now() - timeToMilliseconds(submittedDetails[index].timeTaken);
+    const startTime =
+      Date.now() - timeToMilliseconds(submittedDetails[index].timeTaken);
     setIsRunning(true);
     intervalRef.current = setInterval(() => {
       const elapsedTime = Date.now() - startTime;
@@ -175,13 +147,13 @@ const Stopwatch = () => {
   };
 
   const handleTagSelect = (tagName) => {
-    const updatedTags = tags.map(tag =>
+    const updatedTags = tags.map((tag) =>
       tag.name === tagName ? { ...tag, checked: !tag.checked } : tag
     );
     setTags(updatedTags);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (!task.trim()) {
       alert("Task description is required!");
       return;
@@ -190,27 +162,27 @@ const Stopwatch = () => {
       alert("Project name is required!");
       return;
     }
-    const selectedTags = tags.filter(tag => tag.checked).map(tag => tag.name);
+    const selectedTags = tags
+      .filter((tag) => tag.checked)
+      .map((tag) => tag.name);
 
     const newDetails = {
       projectName,
       task,
       tags: selectedTags,
       timeTaken: formatTime(timeElapsed),
-      createdDate: new Date(),
     };
 
-    console.log('Sending data:', { task, projectName, tags: selectedTags, timeElapsed });
+    console.log("Sending data:", {
+      task,
+      projectName,
+      tags: selectedTags,
+      timeElapsed,
+    });
 
-<<<<<<< HEAD
-    // Simulate API call
-    setTimeout(() => {
-      const projectid = submittedDetails.length + 1; // Simulate project ID generation
-      setSubmittedDetails([...submittedDetails, { ...newDetails, projectid }]);
-=======
     try {
       const response = await axios.post(
-        "https://rmbackend.vercel.app/auth/add_project_data",
+        "https://ten-tuuo.onrender.com/auth/add_project_data",
         {
           task,
           projectName,
@@ -223,22 +195,18 @@ const Stopwatch = () => {
         ...submittedDetails,
         { ...newDetails, projectid: response.data.projectid },
       ]);
->>>>>>> 1cf743038ac42a86793584312849e84d38676e2a
       handleReset();
-    }, 500); // Simulate API delay
+    } catch (error) {
+      console.error("Error adding project:", error);
+    }
   };
 
-  const handleUpdateSubmit = (index) => {
+  const handleUpdateSubmit = async (index) => {
     const detail = submittedDetails[index];
     const projectId = detail.projectid; // Retrieve projectid from project detail object
-<<<<<<< HEAD
-
-    // Simulate API call
-    setTimeout(() => {
-=======
     try {
       await axios.post(
-        `https://rmbackend.vercel.app/auth/update_project_data/${projectId}`,
+        `https://ten-tuuo.onrender.com/auth/update_project_data/${projectId}`,
         {
           projectid: projectId,
           task: detail.task,
@@ -247,9 +215,10 @@ const Stopwatch = () => {
           timeElapsed: timeToMilliseconds(detail.timeTaken),
         }
       );
->>>>>>> 1cf743038ac42a86793584312849e84d38676e2a
       setEditIndex(null);
-    }, 500); // Simulate API delay
+    } catch (error) {
+      console.error("Error updating project:", error);
+    }
   };
 
   const formatTime = (milliseconds) => {
@@ -257,19 +226,13 @@ const Stopwatch = () => {
     const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000);
 
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
   };
 
   const timeToMilliseconds = (timeString) => {
     const [hours, minutes, seconds] = timeString.split(":").map(Number);
     return (hours * 60 * 60 + minutes * 60 + seconds) * 1000;
   };
-
-  const filteredDetails = filterDate
-    ? submittedDetails.filter(
-      detail => detail.createdDate.toDateString() === filterDate.toDateString()
-    )
-    : submittedDetails;
 
   return (
     <>
@@ -281,7 +244,7 @@ const Stopwatch = () => {
             value={task}
             onChange={(e) => setTask(e.target.value)}
             className="tw-border tw-w-full tw-border-gray-500 tw-px-2 tw-py-1 tw-mr-2 tw-flex-1 "
-            style={{ borderBottomRightRadius: 0, borderTopRightRadius: 0, }}
+            style={{ borderBottomRightRadius: 0, borderTopRightRadius: 0 }}
           />
           <div className="tw-relative tw-flex-1">
             <select
@@ -297,20 +260,36 @@ const Stopwatch = () => {
               ))}
             </select>
           </div>
-          <div className="absolute tw-right-0">
+          <div className="absolute tw-right">
             <div
-              className="tw-border tw-border-gray-500 tw-px-2 tw-py-1 tw-cursor-pointer"
+              className="tw-border bf tw-border-gray-500 tw-px-2 tw-py-1 tw-cursor-pointer"
               onClick={handleTagClick}
-              style={{ marginRight: '8px' }}
+              style={{ marginRight: "8px" }}
             >
-              {tags.filter(tag => tag.checked).length > 0 ? tags.filter(tag => tag.checked).slice(0, 3).map(tag => tag.name).join(", ") : <i className="bi bi-tag"></i>}
-              {tags.filter(tag => tag.checked).length > 3 && <span> ...</span>}
+              {tags.filter((tag) => tag.checked).length > 0 ? (
+                tags
+                  .filter((tag) => tag.checked)
+                  .slice(0, 3)
+                  .map((tag) => tag.name)
+                  .join(", ")
+              ) : (
+                <i className="bi bi-tag"></i>
+              )}
+              {tags.filter((tag) => tag.checked).length > 3 && (
+                <span> ...</span>
+              )}
             </div>
             {showDropdown && (
-              <div className="tw-absolute tw-mt-1 tw-z-50 tw-bg-base-300 tw-shadow-md tw-rounded-md" style={{ color: 'black', right: "115px" }}>
+              <div
+                className="tw-absolute tw-mt-1 tw-bg-white tw-shadow-md tw-rounded-md"
+                style={{ color: "black" }}
+              >
                 <ul>
                   {tags.map((tag, index) => (
-                    <li key={index} className="tw-cursor-pointer tw-text-base-content tw-px-3 tw-py-2 tw-hover:bg-gray-200">
+                    <li
+                      key={index}
+                      className="tw-cursor-pointer tw-px-3 tw-py-2 tw-hover:bg-gray-200"
+                    >
                       <input
                         type="checkbox"
                         checked={tag.checked}
@@ -351,7 +330,9 @@ const Stopwatch = () => {
           )}
         </div>
         <div>
-          <h2 className="tw-font-bold">Time Taken: {formatTime(timeElapsed)}</h2>
+          <h2 className="tw-font-bold">
+            Time Taken: {formatTime(timeElapsed)}
+          </h2>
         </div>
         {isRunning && (
           <button
@@ -370,60 +351,48 @@ const Stopwatch = () => {
           </button>
         )}
       </div>
-
       <div className="tw-p-4 tw-overflow-x-auto">
-        <div className="tw-mb-4">
-          <DatePicker
-            selected={filterDate}
-            onChange={(date) => setFilterDate(date)}
-            isClearable
-            placeholderText="Filter date"
-            className="tw-border tw-border-base-content tw-p-2"
-          />
-        </div>
-        <table className="tw-mt-4 tw-border tw-border-base-content tw-w-full">
+        
+        <table className="tw-mt-4 tw-w-full ">
           <thead>
-            <tr>
-              <th className="tw-border tw-p-2 tw-w-1/6">Project Name</th>
-              <th className="tw-border tw-p-2 tw-w-1/6">Description</th>
-              <th className="tw-border tw-p-2 tw-w-1/6">Tag</th>
-              <th className="tw-border tw-p-2 tw-w-1/6">Time Taken</th>
-              <th className="tw-border tw-p-2 tw-w-1/6">Created Date</th>
-              <th className="tw-border tw-p-2 tw-w-1/6">Actions</th>
+            <tr className=" tw-text-black ">
+              <th className="tw-p-2 tw-w-1/5">Project Name</th>
+              <th className="tw-p-2 tw-w-1/5">Description</th>
+              <th className="tw-p-2 tw-w-1/5">Tag</th>
+              <th className="tw-p-2 tw-w-1/5">Time Taken</th>
+              <th className="tw-p-2 tw-w-1/5">Actions</th>
             </tr>
           </thead>
+
           <tbody>
-            {filteredDetails.map((detail, index) => (
+            {submittedDetails.map((detail, index) => (
               <tr
                 key={index}
-                onMouseEnter={() => setHoveredRowIndex(index)}
-                onMouseLeave={() => {
-                  setHoveredRowIndex(null);
-                  setHoveredColumnIndex(null);
-                }}
-                className="tw-relative"
+                className="tw-last:border-b-0 tw-transition-colors tw-duration-300 tw-hover:bg-gray-100"
               >
-                <td className="tw-border tw-p-2 tw-w-1/6">{detail.projectName}</td>
-                <td className="tw-border tw-p-2 tw-w-1/6">{detail.task}</td>
-                <td
-                  className="tw-border tw-p-2 tw-w-1/6"
-                  onMouseEnter={() => setHoveredColumnIndex(index)}
-                >
-                  {detail.tags.slice(0, 2).join(", ")}
-                  {detail.tags.length > 2 && <span> ...</span>}
-                  {hoveredRowIndex === index && hoveredColumnIndex === index && (
-                    <div className="tw-absolute tw-border tw-border-base-content tw-bg-base-300 tw-text-base-content  tw-p-2 tw-mt-2 tw-z-10" style={{ top: '-20%', left: '45%', transform: 'translateX(-50%)', minWidth: '100px' }}>
-                      {detail.tags.map((tag, i) => (
-                        <span key={i} className="tw-text-xs tw-bg-transparent tw-block">
-                          {tag}
-                        </span>
-                      ))}
-                    </div>
-                  )}
-                </td>
-                <td className="tw-border tw-p-2 tw-w-1/6">{detail.timeTaken}</td>
-                <td className="tw-border tw-p-2 tw-w-1/6">{detail.createdDate.toDateString()}</td>
-                <td className="tw-border tw-p-2 tw-w-1/6">
+                <td className="tw-p-2 tw-w-1/5">{detail.projectName}</td>
+                <td className="tw-p-2 tw-w-1/5">{detail.task}</td>
+                <td className="tw-p-2 tw-w-1/5">
+                {(expandedRows[index] ? detail.tags : detail.tags.slice(0, 3)).map((tag, tagIndex) => (
+                  <span
+                    key={tagIndex}
+                    className="tw-bg-gray-300 tw-text-black tw-font-medium tw-py-1 tw-px-2 tw-mr-1 tw-mb-1 tw-rounded-xl"
+                  >
+                    {tag}
+                  </span>
+                ))}
+                {detail.tags.length > 3 && !expandedRows[index] && (
+                  <span
+                    className="tw-bg-gray-300 tw-text-black tw-font-medium tw-py-1 tw-px-2 tw-mr-1 tw-mb-1 tw-rounded-xl cursor-pointer"
+                    onClick={() => toggleRowExpansion(index)}
+                  >
+                    ...
+                  </span>
+                )}
+              </td>
+
+                <td className="tw-p-2 tw-w-1/5">{detail.timeTaken}</td>
+                <td className="tw-p-2 tw-w-1/5">
                   {editIndex !== index ? (
                     <button
                       onClick={() => handleRun(index)}
