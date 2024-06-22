a82d5907de0ca3f91911bdc4e926bcd8948d441f
salaudeen22 <salaudeensalu@gmail.com>
2024-06-22 09:28:58 +0530

    reverting back to 1cf743038ac42a86793584312849e84d38676e2a because of conflict error commit

--- EMPLOYEE-frontend/src/Components/AdminDashboard.jsx
+++ EMPLOYEE-frontend/src/Components/AdminDashboard.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-// import React, { useState } from "react";
-// import { Link, useNavigate } from "react-router-dom";
-=======
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -9,12 +5,12 @@ import axios from "axios";
 import Navbar from "./Navbar";
 
 const AdminDashboard = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate();q
   axios.defaults.withCredentials = true;
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const handleLogout = () => {
-    axios.get("https://rmbackend.vercel.app/auth/logout").then((result) => {
+    axios.get("https://ten-tuuo.onrender.com/auth/logout").then((result) => {
       if (result.data.Status) {
         navigate("adminlogin");
       }
@@ -192,275 +188,98 @@ export default AdminDashboard;
 
 // import React from "react";
 // import { Link, Outlet, useNavigate } from "react-router-dom";
->>>>>>> 1cf743038ac42a86793584312849e84d38676e2a
 // import "bootstrap-icons/font/bootstrap-icons.css";
 // import axios from "axios";
-// import Navbar from "./Navbar";
 
 // const AdminDashboard = () => {
 //   const navigate = useNavigate();
 //   axios.defaults.withCredentials = true;
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-
 //   const handleLogout = () => {
-//     axios.get("https://rmbackend.vercel.app/auth/logout").then((result) => {
+//     axios.get("https://ten-tuuo.onrender.com/auth/logout").then((result) => {
 //       if (result.data.Status) {
 //         navigate("adminlogin");
 //       }
 //     });
 //   };
-
 //   return (
-//     <div className="container-fluid min-vh-100 tw-bg-base-300 tw-text-base-content ">
-//       <div className="row flex-nowrap tw-h-screen">
-//         <div className="col-auto col-2 col-md-3 col-xl-2 px-0 hover:bg-gray-900">
-
-//           <div className>
-//             <div className="d-flex bg-dark col-2  col-md-3 col-xl-2 px-0 flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 tw-fixed tw-h-screen">
-//               <Link
-//                 to="/admindashboard"
-//                 className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none "
-//               >
-//                 <span className="fs-5 fw-bolder d-none d-sm-inline">
-//                   Admin Panel
-//                 </span>
-//               </Link>
-//               <ul
-//                 className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-//                 id="menu"
-//               >
-//                  <li className="w-100">
+//     <div className="container-fluid min-vh-100 ">
+//       <div className="row">
+//         <div className="col-md-3 col-lg-2 bg-dark px-0">
+//           <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+//             <Link
+//               to="/admindashboard"
+//               className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none"
+//             >
+//               <span className="fs-5 fw-bolder d-none d-sm-inline">
+//                 Admin Panel
+//               </span>
+//             </Link>
+//             <ul className="nav nav-pills flex-column mb-sm-auto mb-0">
+//               <li>
 //                 <Link
-//                   to="/admindashboard"
-//                   className="nav-link text-white px-0 align-middle hover:text-primary "
-//                 >
-//                   <i className="fs-9 bi-speedometer2 ms-2"></i>
-//                   <span className="ms-2 d-none d-sm-inline">Dashboard</span>
-//                 </Link>
-//               </li>
-//                 <li className="w-100">
-//                   <Link to="/admindashboard/employee"
-//                     className="nav-link text-white px-0 align-middle">
-//                     <i className="fs-9 bi bi-people ms-2"></i>
-//                     <span className="ms-2 d-none d-sm-inline"> Manage Employee</span>
-//                   </Link>
-//                 </li>
-//                 <li className="w-100">
-//                   <Link to="/admindashboard/employeelist" className="nav-link text-white px-0 align-middle">
-//                     <i className="fs-9 bi bi-card-list ms-2"></i>
-//                     <span className="ms-2 d-none d-sm-inline"> Employee Details</span>
-//                   </Link>
-//                 </li>
-//                 <li className="w-100">
-//                   <Link to="/admindashboard/employeesec" className="nav-link text-white px-0 align-middle">
-//                   <i class="bi bi-person ms-2"></i>
-
-//                     <span className="ms-2 d-none d-sm-inline">Employee sec</span>
-//                   </Link>
-//                 </li>
-//                 <li className="w-100">
-//                 <Link
-//                   to="/admindashboard/projectmanagement"
+//                   to="/admindashboard/employee"
 //                   className="nav-link text-white px-0 align-middle"
 //                 >
-//                   <i className="fs-9 bi bi-file-earmark ms-2"></i>
-//                   <span className="ms-2 d-none d-sm-inline">Project</span>
+//                   <i className="fs-4 bi-people me-2"></i>
+//                   <span className="d-none d-sm-inline">Manage Employee</span>
 //                 </Link>
 //               </li>
-
-//                 <li className="w-100">
-//                   <Link to="/admindashboard/addproject" className="nav-link text-white px-0 align-middle">
-//                     <i className="fs-9 bi bi-plus-circle ms-2"></i>
-//                     <span className="ms-2 d-none d-sm-inline">  Add Project</span>
-//                   </Link>
-//                 </li>
-//                 <li className="w-100">
-//                   <Link to="/admindashboard/tag" className="nav-link text-white px-0 align-middle">
-//                     <i className="fs-9 bi bi-tag ms-2"></i>
-//                     <span className="ms-2 d-none d-sm-inline">  Tag</span>
-//                   </Link>
-//                 </li>
-//                 <li className="w-100">
-//                   <Link to="/admindashboard/CreateMeeting" className="nav-link text-white px-0 align-middle">
-//                     <i className="fs-9 bi bi-calendar-plus ms-2"></i>
-//                     <span className="ms-2 d-none d-sm-inline">  Create Meeting</span>
-//                   </Link>
-//                 </li>
-
-//                 <li className="w-100" onClick={handleLogout}>
-//                   <Link to="/adminlogin" className="nav-link text-white px-0 align-middle">
-//                     <i className="fs-9 bi bi-box-arrow-right ms-2"></i>
-//                     <span className="ms-2 d-none d-sm-inline"> Logout</span>
-//                   </Link>
-//                 </li>
-//               </ul>
-//             </div>
+//               <li>
+//                 <Link
+//                   to="/admindashboard/employeelist"
+//                   className="nav-link text-white px-0 align-middle"
+//                 >
+//                   <i className="fs-4 bi-people me-2"></i>
+//                   <span className="d-none d-sm-inline">Employee Details</span>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link
+//                   to="/admindashboard/addproject"
+//                   className="nav-link text-white px-0 align-middle"
+//                 >
+//                   <i className="fs-4 bi-people me-2"></i>
+//                   <span className="d-none d-sm-inline">Add Project</span>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link
+//                   to="/admindashboard/tag"
+//                   className="nav-link text-white px-0 align-middle"
+//                 >
+//                   <i className="fs-4 bi bi-tag me-2"></i>
+//                   <span className="d-none d-sm-inline">Tag</span>
+//                 </Link>
+//               </li>
+//               <li>
+//                 <Link
+//                   to="/admindashboard/CreateMeeting"
+//                   className="nav-link text-white px-0 align-middle"
+//                 >
+//                   <i className="fs-4 bi bi-tag me-2"></i>
+//                   <span className="d-none d-sm-inline">Create Meeting</span>
+//                 </Link>
+//               </li>
+//               <li onClick={handleLogout}>
+//                 <Link
+//                   to="/adminlogin"
+//                   className="nav-link text-white px-0 align-middle"
+//                 >
+//                   <i className="fs-4 bi-power me-2"></i>
+//                   <span className="d-none d-sm-inline">Logout</span>
+//                 </Link>
+//               </li>
+//             </ul>
 //           </div>
 //         </div>
-
-//         {/* Main Content */}
-
-//           <Navbar />
-//           {/* Your main content goes here */}
-
+//         <div className="col-md-9 col-lg-10">
+//           <Outlet />
+//         </div>
 //       </div>
 //     </div>
 //   );
 // };
 
 // export default AdminDashboard;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import axios from "axios";
-import Navbar from "./Navbar";
-
-const AdminDashboard = () => {
-  const navigate = useNavigate();
-  axios.defaults.withCredentials = true;
-  const [activeLink, setActiveLink] = useState(window.location.pathname);
-
-  const handleLogout = () => {
-    axios.get("https://rmbackend.vercel.app/auth/logout").then((result) => {
-      if (result.data.Status) {
-        navigate("adminlogin");
-      }
-    });
-  };
-
-  const handleNavClick = (path) => {
-    setActiveLink(path);
-    navigate(path);
-  };
-
-  return (
-    <div className="container-fluid min-vh-100 tw-bg-base-300 tw-text-base-content">
-      <div className="row flex-nowrap ">
-        <div className="col-auto col-2 col-md-3 col-xl-2 px-0 tw-h-screen overflow-y-auto sm:h-full">
-          <div className="d-flex bg-dark col-2 col-md-3 col-xl-2 px-sm-2 px-0 flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 tw-fixed sm:tw-h-full">
-            <Link
-              to="/admindashboard"
-              className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none tw-text-xs md:tw-text-xl"
-            >
-              <span className="fs-9 fw-bolder d-none d-sm-inline">Admin Panel</span>
-            </Link>
-            <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard" ? "tw-bg-gray-300" : ""}`}>
-                <Link
-                  to="/admindashboard"
-                  className={`nav-link ${activeLink === "/admindashboard" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
-                  onClick={() => handleNavClick('/admindashboard')}
-                >
-                  <i className="fs-9 bi-speedometer2 ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Dashboard</span>
-                </Link>
-              </li>
-
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/employee" ? "tw-bg-gray-300" : ""}`}>
-                <Link
-                  to="/admindashboard/employee"
-                  className={`nav-link ${activeLink === "/admindashboard/employee" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
-                  onClick={() => handleNavClick('/admindashboard/employee')}
-                >
-                  <i className="fs-9 bi bi-people ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Manage Employee</span>
-                </Link>
-              </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/employeelist" ? "tw-bg-gray-300" : ""}`}>
-                <Link
-                  to="/admindashboard/employeelist"
-                  className={`nav-link ${activeLink === "/admindashboard/employeelist" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
-                  onClick={() => handleNavClick('/admindashboard/employeelist')}
-                >
-                  <i className="fs-9 bi bi-card-list ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Employee Details</span>
-                </Link>
-              </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/employeesec" ? "tw-bg-gray-300" : ""}`}>
-                <Link
-                  to="/admindashboard/employeesec"
-                  className={`nav-link ${activeLink === "/admindashboard/employeesec" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
-                  onClick={() => handleNavClick('/admindashboard/employeesec')}
-                >
-                  <i className="bi bi-person ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Employee Sec</span>
-                </Link>
-              </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/projectmanagement" ? "tw-bg-gray-300" : ""}`}>
-                <Link
-                  to="/admindashboard/projectmanagement"
-                  className={`nav-link ${activeLink === "/admindashboard/projectmanagement" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
-                  onClick={() => handleNavClick('/admindashboard/projectmanagement')}
-                >
-                  <i className="fs-9 bi bi-file-earmark ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Project</span>
-                </Link>
-              </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/addproject" ? "tw-bg-gray-300" : ""}`}>
-                <Link
-                  to="/admindashboard/addproject"
-                  className={`nav-link ${activeLink === "/admindashboard/addproject" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
-                  onClick={() => handleNavClick('/admindashboard/addproject')}
-                >
-                  <i className="fs-9 bi bi-plus-circle ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Add Project</span>
-                </Link>
-              </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/tag" ? "tw-bg-gray-300" : ""}`}>
-                <Link
-                  to="/admindashboard/tag"
-                  className={`nav-link ${activeLink === "/admindashboard/tag" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
-                  onClick={() => handleNavClick('/admindashboard/tag')}
-                >
-                  <i className="fs-9 bi bi-tag ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Tag</span>
-                </Link>
-              </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/CreateMeeting" ? "tw-bg-gray-300" : ""}`}>
-                <Link
-                  to="/admindashboard/CreateMeeting"
-                  className={`nav-link ${activeLink === "/admindashboard/CreateMeeting" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
-                  onClick={() => handleNavClick('/admindashboard/CreateMeeting')}
-                >
-                  <i className="fs-9 bi bi-calendar-plus ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Create Meeting</span>
-                </Link>
-              </li>
-              <li className="w-100 hover:tw-bg-gray-300 tw-rounded" onClick={handleLogout}>
-                <Link
-                  to="/adminlogin"
-                  className="nav-link text-white px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black"
-                >
-                  <i className="fs-9 bi bi-box-arrow-right ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Logout</span>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <Navbar />
-      </div>
-    </div>
-  );
-};
-
-export default AdminDashboard;
-
-
 
 
