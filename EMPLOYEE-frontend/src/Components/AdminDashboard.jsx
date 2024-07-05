d4afec155e22ddabc784a83c77e171f2a70cc803
Rushikesh Sanjay Deshmukh <rushideshmukh3620@gmail.com>
2024-07-05 17:45:31 +0530

    dashboard.jsx and adminDashboard.jsx update by Kumar Shanoo

--- EMPLOYEE-frontend/src/Components/AdminDashboard.jsx
+++ EMPLOYEE-frontend/src/Components/AdminDashboard.jsx
@@ -26,7 +26,7 @@ const AdminDashboard = () => {
     <div className="container-fluid min-vh-100 tw-bg-base-300 tw-text-base-content">
       <div className="row flex-nowrap ">
         <div className="col-auto col-2 col-md-3 col-xl-2 px-0 tw-h-screen overflow-y-auto sm:h-full">
-          <div className="d-flex bg-dark col-2 col-md-3 col-xl-2 px-sm-2 px-0 flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 tw-fixed sm:tw-h-full">
+          <div className="d-flex bg-dark col-2 col-md-3 col-xl-2 px-sm-2 px-0 flex-column  px-3 pt-2 text-white min-vh-100 tw-fixed sm:tw-h-full">
             <Link
               to="/admindashboard"
               className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none tw-text-xs md:tw-text-xl"
@@ -34,94 +34,94 @@ const AdminDashboard = () => {
               <span className="fs-9 fw-bolder d-none d-sm-inline">Admin Panel</span>
             </Link>
             <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/admindashboard" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/admindashboard"
                   className={`nav-link ${activeLink === "/admindashboard" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/admindashboard')}
                 >
-                  <i className="fs-9 bi-speedometer2 ms-2"></i>
+                  <i className="fs-9 bi-speedometer2 ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Dashboard</span>
                 </Link>
               </li>
               
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/adminlist" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/admindashboard/adminlist" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/admindashboard/adminlist"
                   className={`nav-link ${activeLink === "/admindashboard/adminlist" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/admindashboard/adminlist')}
                 >
-                 <i class="bi bi-person-check ms-2"></i>
+                 <i class="bi bi-person-check ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Admin Details</span>
                 </Link>
               </li>
 
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/employeelist" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/admindashboard/employeelist" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/admindashboard/employeelist"
                   className={`nav-link ${activeLink === "/admindashboard/employeelist" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/admindashboard/employeelist')}
                 >
-                  <i className="fs-9 bi bi-card-list ms-2"></i>
+                  <i className="fs-9 bi bi-card-list ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Employee Details</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/employeesec" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/admindashboard/employeesec" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/admindashboard/employeesec"
                   className={`nav-link ${activeLink === "/admindashboard/employeesec" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/admindashboard/employeesec')}
                 >
-                  <i className="bi bi-person ms-2"></i>
+                  <i className="bi bi-person ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Employee Sec</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/projectmanagement" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/admindashboard/projectmanagement" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/admindashboard/projectmanagement"
                   className={`nav-link ${activeLink === "/admindashboard/projectmanagement" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/admindashboard/projectmanagement')}
                 >
-                  <i className="fs-9 bi bi-file-earmark ms-2"></i>
+                  <i className="fs-9 bi bi-file-earmark ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Project</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/addproject" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/admindashboard/addproject" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/admindashboard/addproject"
                   className={`nav-link ${activeLink === "/admindashboard/addproject" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/admindashboard/addproject')}
                 >
-                  <i className="fs-9 bi bi-plus-circle ms-2"></i>
+                  <i className="fs-9 bi bi-plus-circle ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Add Project</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/tag" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/admindashboard/tag" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/admindashboard/tag"
                   className={`nav-link ${activeLink === "/admindashboard/tag" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/admindashboard/tag')}
                 >
-                  <i className="fs-9 bi bi-tag ms-2"></i>
+                  <i className="fs-9 bi bi-tag ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Tag</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/admindashboard/CreateMeeting" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/admindashboard/CreateMeeting" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/admindashboard/CreateMeeting"
                   className={`nav-link ${activeLink === "/admindashboard/CreateMeeting" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/admindashboard/CreateMeeting')}
                 >
-                  <i className="fs-9 bi bi-calendar-plus ms-2"></i>
+                  <i className="fs-9 bi bi-calendar-plus ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Create Meeting</span>
                 </Link>
               </li>
-              <li className="w-100 hover:tw-bg-gray-300 tw-rounded" onClick={handleLogout}>
+              <li className="w-100 hover:tw-bg-gray-300 mb-1 tw-rounded" onClick={handleLogout}>
                 <Link
                   to="/adminlogin"
                   className="nav-link text-white px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black"
                 >
-                  <i className="fs-9 bi bi-box-arrow-right ms-2"></i>
+                  <i className="fs-9 bi bi-box-arrow-right ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Logout</span>
                 </Link>
               </li>
--- EMPLOYEE-frontend/src/Components/Dashboard.jsx
+++ EMPLOYEE-frontend/src/Components/Dashboard.jsx
@@ -60,7 +60,7 @@ const Dashboard = () => {
     <div className="container-fluid tw-bg-base-300 tw-text-base-content tw-h-screen sm:tw-h-full">
       <div className="row flex-nowrap p-0 ">
         <div className="col-auto col-2 col-md-3 col-xl-2 px-xxl-2 px-0 tw-h-screen overflow-y-auto sm:h-full">
-          <div className="d-flex bg-dark col-2 col-md-3 col-xl-2 px-sm-2 px-0 flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100 tw-fixed sm:tw-h-full">
+          <div className="d-flex bg-dark col-2 col-md-3 col-xl-2 px-sm-2 px-0 flex-column  px-3 pt-2 text-white min-vh-100 tw-fixed sm:tw-h-full">
             <Link
               to="/dashboard"
               className="d-flex align-items-center pb-3 mb-md-1 mt-md-3 me-md-auto text-white text-decoration-none tw-text-xs md:tw-text-xl"
@@ -73,82 +73,82 @@ const Dashboard = () => {
               className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
               id="menu"
             >
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/dashboard" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/dashboard" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/dashboard"
                   className={`nav-link ${activeLink === "/dashboard" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/dashboard')}
                 >
-                  <i className="fs-9 bi-speedometer2 ms-2"></i>
+                  <i className="fs-9 bi-speedometer2 ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Dashboard</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/dashboard/timesheet" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/dashboard/timesheet" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/dashboard/timesheet"
                   className={`nav-link ${activeLink === "/dashboard/timesheet" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/dashboard/timesheet')}
                 >
-                  <i className="fs-9 bi bi-calendar3 ms-2"></i>
+                  <i className="fs-9 bi bi-calendar3 ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline"> Timesheet</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/dashboard/timetracker" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/dashboard/timetracker" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/dashboard/timetracker"
                   className={`nav-link ${activeLink === "/dashboard/timetracker" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/dashboard/timetracker')}
                 >
-                  <i className="fs-9 bi bi-clock ms-2"></i>
+                  <i className="fs-9 bi bi-clock ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline"> TimeTracker</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/dashboard/calender" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/dashboard/calender" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/dashboard/calender"
                   className={`nav-link ${activeLink === "/dashboard/calender" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/dashboard/calender')}
                 >
-                  <i className="fs-9 bi bi-calendar ms-2"></i>
+                  <i className="fs-9 bi bi-calendar ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Calendar</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/dashboard/reports" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/dashboard/reports" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/dashboard/reports"
                   className={`nav-link ${activeLink === "/dashboard/reports" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/dashboard/reports')}
                 >
-                  <i className="fs-9 bi bi-bar-chart-fill ms-2"></i>
+                  <i className="fs-9 bi bi-bar-chart-fill ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Report</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/dashboard/profile" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/dashboard/profile" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/dashboard/profile"
                   className={`nav-link ${activeLink === "/dashboard/profile" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/dashboard/profile')}
                 >
-                  <i className="fs-9 bi-person ms-2"></i>
+                  <i className="fs-9 bi-person ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Profile</span>
                 </Link>
               </li>
-              <li className={`w-100 hover:tw-bg-gray-300 tw-rounded ${activeLink === "/dashboard/leave" ? "tw-bg-gray-300" : ""}`}>
+              <li className={`w-100 hover:tw-bg-gray-300 mb-1 tw-rounded ${activeLink === "/dashboard/leave" ? "tw-bg-gray-300" : ""}`}>
                 <Link
                   to="/dashboard/leave"
                   className={`nav-link ${activeLink === "/dashboard/leave" ? "tw-text-black" : "text-white"} px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black`}
                   onClick={() => handleNavClick('/dashboard/leave')}
                 >
-                  <i className="fs-9 bi bi-house-heart ms-2"></i>
+                  <i className="fs-9 bi bi-house-heart ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Leave</span>
                 </Link>
               </li>
-              <li className="w-100 hover:tw-bg-gray-300 tw-rounded" onClick={handleLogout}>
+              <li className="w-100 hover:tw-bg-gray-300 mb-1 tw-rounded" onClick={handleLogout}>
                 <Link
                   to="/"
                   className="nav-link text-white px-0 align-middle tw-text-xs md:tw-text-xl flex items-center justify-center hover:tw-text-black"
                 >
-                  <i className="fs-9 bi-power ms-2"></i>
+                  <i className="fs-9 bi-power ms-3"></i>
                   <span className="ms-2 d-none d-sm-inline">Logout</span>
                 </Link>
               </li>
